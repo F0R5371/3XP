da0bf2ea237b2c3403edc846d04abfc78fc759d6
F0R5371 <aiden.zavaleta@proton.me>
2026-03-30 15:25:27 -0500

    Tracking hand coordinates so that we can later use a gradient to change sound based on hands position

--- python/vision.py
+++ python/vision.py
@@ -18,10 +18,34 @@ def start_video(model):
         ret, frame = cap.read()
         results = model(frame, stream = True)
     
-    
         for r in results:
             
-            print(dir(r.keypoints))
+            """
+            Results is a generator. Typically, though, we only have one thing being detected so in this case we only care about r (the one hand).
+            If we take this one hand, and go through its keypoints (a Tensor) we can find each xy (coordinate). With keypoints.xy, we get a tensor of 
+            shape [0, 21, 2] which means that for each 21 keypoints, we have a tensor of 2 with x and y coordinates.
+            """
+            
+            #print(r.keypoints.xy[0][0])
+            
+            """
+            Now we also need the position of the whole hand. We can use the xyxy attribute to get two opposite corners of the box.
+            """
+            
+            #print(r.boxes.xyxy[0][0], r.boxes.xyxy[0][1], r.boxes.xyxy[0][2], r.boxes.xyxy[0][3])
+            #print(r.boxes.xyxy)
+            
+            bbox = r.boxes.xyxy.cpu().numpy()
+            
+            if bbox.shape[0] > 0:
+                
+                x_coords = [bbox[0][0], bbox[0][2]]
+                y_coords = [bbox[0][1], bbox[0][3]]
+                
+                mid = (sum(x_coords) / 2.0, sum(y_coords) / 2.0)
+                
+                print(mid)
+        
             
             detected = r.plot()
             cv.imshow("Webcam", detected)
